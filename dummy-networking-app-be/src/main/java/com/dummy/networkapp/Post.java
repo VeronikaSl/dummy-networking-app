5123dc79aeb81e7ada760cce8e8e182ef5d68295
Daniel Altena <miao@noisefever.de>
2022-05-06 09:28:08 +0200

    1. Versuch den Postlink herzustellen. Ungetestet leider. Programm läuft, aber ich muss noch Posts generieren.

--- dummy-networking-app-be/src/main/java/com/dummy/networkapp/Post.java
+++ dummy-networking-app-be/src/main/java/com/dummy/networkapp/Post.java
@@ -1,10 +1,16 @@
 package com.dummy.networkapp;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +31,16 @@ public class Post {
 	@Column(name = "ref_post_id")
 	private long refPostId;
 	
+	// Post, auf das geantwortet wurde
+	@ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name = "ref_post_id", insertable = false, updatable = false)
+	private Post referencePost;
+	
+	// Antwortposts
+	@OneToMany(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name = "id", insertable = false, updatable = false)
+	private List<Post> answers;
+	
 	@Column(name = "likes")
 	private int likes;
 	
@@ -32,10 +48,25 @@ public class Post {
 		
 	}
 	
-	public Post(String user, String message, int refPostId) {
+	public Post(String user, String message) {
 		this.user = user;
 		this.message = message;
-		this.refPostId = refPostId;
+	}
+
+	public Post getReferencePost() {
+		return referencePost;
+	}
+
+	public void setReferencePost(Post referencePost) {
+		this.referencePost = referencePost;
+	}
+
+	public List<Post> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Post> answers) {
+		this.answers = answers;
 	}
 
 	public long getId() {
